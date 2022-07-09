375e1f7cc5932a7ad90a6350bf741ef3ede0710a
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-07-09 17:43:31 -0400

    reverted from cloudinary changes

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,38 +1,30 @@
-const cloudinary = require('cloudinary').v2;
-
-function addPostHandler(event) {
+async function addPostHandler(event) {
   event.preventDefault();
 
   const description = document.querySelector('input[id="post-description"]').value;
-  const image = document.querySelector('input[id="post-filename"]').value;
+  const filename = document.querySelector('input[id="post-filename"]').value;
   const blog_body = document.querySelector('input[id="post-blog-body"]').value;
   const title = document.querySelector('input[id="post-title"]').value;
 
-  cloudinary.uploader.upload(image)
-    .then(result => {
-      const filename = (result.url)
 
-      fetch('/api/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-          description,
-          filename,
-          blog_body,
-          title
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          document.location.reload('dashboard');
-        } else {
-          alert(response.statusText);
-        }
-      })
-    })
-    .catch(error => { console.log(error) });
+  const response = await fetch('/api/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      description,
+      filename,
+      blog_body,
+      title
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 
+  if (response.ok) {
+    document.location.reload('dashboard');
+  } else {
+    alert(response.statusText);
+  }
 }
 
 document.querySelector('.blog-form').addEventListener('submit', addPostHandler);
